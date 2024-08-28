865a3df1a82331149da16713d27ca47403c83181
dunsolt <danvxlhydra@hotmail.com>
2024-08-28 10:00:24 +0100

    0.1

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
-print("Starting asteriods!")
+import pygame 
+from constants import *
+
+print("Starting asteroids!")
+
+def main():
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
 if __name__ == "__main__":
     main()
+
