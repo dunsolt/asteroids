0601e578e69865ebd93f439b2350d0a86a5341da
dunsolt <danvxlhydra@hotmail.com>
2024-08-29 13:16:05 +0100

    groups

--- main.py
+++ main.py
@@ -10,22 +10,23 @@ print("Starting asteroids!")
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable) 
+    player = Player(x=(SCREEN_WIDTH/2), y=(SCREEN_HEIGHT/2), PLAYER_RADIUS=PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
-player = Player(x=(SCREEN_WIDTH/2), y=(SCREEN_HEIGHT/2), PLAYER_RADIUS=PLAYER_RADIUS)
-
-
 
 if __name__ == "__main__":
     main()
