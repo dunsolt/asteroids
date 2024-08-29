3c5dd4b10692150af6cb719e4c31f04f7d5742c6
dunsolt <danvxlhydra@hotmail.com>
2024-08-29 11:35:06 +0100

    triangle added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame 
 from constants import *
+from player import Player
 
 print("Starting asteroids!")
 
@@ -10,15 +11,20 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+player = Player(x=(SCREEN_WIDTH/2), y=(SCREEN_HEIGHT/2), PLAYER_RADIUS=PLAYER_RADIUS)
+
 
 
 if __name__ == "__main__":
